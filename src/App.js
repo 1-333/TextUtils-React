069df7395847e5a260bcebd68eff08d868a37b6b
Aditya Todi <adityatodi.surat@gmail.com>
2024-05-24 17:13:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { useState, useSyncExternalStore } from 'react';
 import Navbar from './components/Navbar';
 import Alerts from './components/Alerts';
 import Textform from './components/Textform';
-// import About from './components/About';
-// import {
-//   Routes,
-//   Route,
-// } from 'react-router-dom';
+import About from './components/About';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -39,11 +39,11 @@ function App() {
         <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode}/>
         <Alerts alert={alert}/>
         <div className="container my-5">
-        {/* <Routes> */}
-          {/* <Route path="/about" element={<About mode={mode}/>} /> */}
-          {/* <Route path="/" element={<Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter, Remove extra spaces' mode={mode}/>} /> */}
-          {<Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter, Remove extra spaces' mode={mode}/>}
-        {/* </Routes> */}
+        <Routes>
+          <Route path="/about" element={<About mode={mode}/>} />
+          <Route path="/" element={<Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter, Remove extra spaces' mode={mode}/>} /> 
+          {/* {<Textform showAlert={showAlert} heading='Try TextUtils - Word Counter, Character Counter, Remove extra spaces' mode={mode}/>} */}
+        </Routes>
         </div>
         
     </>
